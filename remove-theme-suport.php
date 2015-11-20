25f80c9fb8cdfd2900a42304c560b70cbe19a72b
Anderson <theandersonn@gmail.com>
2015-11-20 18:00:28 -0200

    insere filter que remove aviso de atualização de plugins

--- remove-theme-suport.php
+++ remove-theme-suport.php
@@ -3,5 +3,9 @@
 	REMOVE FUNCIONALIDADE(s) DO TEMA
 --------------------------------------------------------------*/
 
+// aviso de atualização de plugins - remove
+remove_action('load-update-core.php','wp_update_plugins');
+add_filter('pre_site_transient_update_plugins','__return_null');
+
 // aviso de atualização de versão do WordPress - remove
 add_filter( 'pre_site_transient_update_core' , create_function( '$a', "return null;" ) );

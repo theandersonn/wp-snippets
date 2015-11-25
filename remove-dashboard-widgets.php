484fa6c54b332011219155445c98c1f5919a6fee
Anderson <theandersonn@gmail.com>
2015-11-25 01:29:50 -0200

    Insere função - remove_dashboard_widgets

---
+++ remove-dashboard-widgets.php
@@ -0,0 +1,20 @@
+<?php
+/*--------------------------------------------------------------
+DESATIVAR WIDGETS DO DASHBOARD ADMIN PADRÃO
+--------------------------------------------------------------*/
+add_action('wp_dashboard_setup', 'btwp_remove_dashboard_widgets');
+
+function btwp_remove_dashboard_widgets() {
+
+	global $wp_meta_boxes;
+
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+}

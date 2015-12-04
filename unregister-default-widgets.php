ee6f19340ec048ecb3fefcbcf0509fe58651c213
Anderson <theandersonn@gmail.com>
2015-12-04 01:05:24 -0200

    Insere função unregister-default-widgets

---
+++ unregister-default-widgets.php
@@ -0,0 +1,20 @@
+<?php
+
+/*--------------------------------------------------------------
+	UNREGISTER ALL DEFAULT WP WIDGETS
+--------------------------------------------------------------*/
+function btwp_unregister_default_widgets() {
+    unregister_widget('WP_Widget_Pages');
+    unregister_widget('WP_Widget_Calendar');
+    unregister_widget('WP_Widget_Archives');
+    unregister_widget('WP_Widget_Links');
+    unregister_widget('WP_Widget_Meta');
+    unregister_widget('WP_Widget_Search');
+    unregister_widget('WP_Widget_Text');
+    unregister_widget('WP_Widget_Categories');
+    unregister_widget('WP_Widget_Recent_Posts');
+    unregister_widget('WP_Widget_Recent_Comments');
+    unregister_widget('WP_Widget_RSS');
+    unregister_widget('WP_Widget_Tag_Cloud');
+}
+add_action('widgets_init', 'btwp_unregister_default_widgets', 1);

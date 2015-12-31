d3bdd1d90a707398a832bd3add0f7feca1f65147
Anderson <theandersonn@gmail.com>
2015-12-30 23:57:23 -0200

    Remove show_admin_bar

--- theme-suport/remove-junk-header.php
+++ theme-suport/remove-junk-header.php
@@ -15,5 +15,3 @@ remove_action('wp_head', 'parent_post_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
 remove_action('wp_head', 'print_emoji_detection_script', 7 );
 remove_action('wp_print_styles', 'print_emoji_styles' );
-
-add_filter('show_admin_bar', '__return_false');

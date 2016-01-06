0016a76c3ff88fd5c3b0a39f83f5ea842eb82bda
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2016-01-06 17:28:02 -0200

    Insert remove-junk-header > emojis

--- theme-suport/remove-junk-header.php
+++ theme-suport/remove-junk-header.php
@@ -13,5 +13,15 @@ remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'start_post_rel_link', 10, 0);
 remove_action('wp_head', 'parent_post_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+
+// disable emojis
 remove_action('wp_head', 'print_emoji_detection_script', 7 );
 remove_action('wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+
+// disable admin bar
+add_filter('show_admin_bar', '__return_false');

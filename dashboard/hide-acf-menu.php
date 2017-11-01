8d134a34843248446065c08de2f98b91fc6f055b
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2017-11-01 17:53:29 -0200

    Update btwp_hide_acf_menu

--- dashboard/hide-acf-menu.php
+++ dashboard/hide-acf-menu.php
@@ -6,16 +6,7 @@ add_action( 'admin_menu', 'btwp_hide_acf_menu', 999 );
 
 function btwp_hide_acf_menu(){
 
-    // users can view the menu
-    $admins = array( 
-        'name-admin'
-    );
-
-    // get the current user
-    $current_user = wp_get_current_user();
-
-    // match and remove if needed
-    if( !in_array( $current_user->user_login, $admins ) )
+    if( !(current_user_can('administrator')) )
     {
         remove_menu_page('edit.php?post_type=acf');
     }
